5a84ccbb830c93d9770c6a698149c4a145afd735
Michael Mohr <akihana@gmail.com>
2016-09-07 21:51:17 -0700

    Add 64-bit support

--- openssl/build_til.sh
+++ openssl/build_til.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-IDA="${HOME}/ida-6.9"
+IDA="/opt/ida-6.95"
 if [ ! -d "${IDA}" ]
 then
   echo "Edit the IDA path in this script."
   exit 1
 fi
 
-${IDA}/tilib -v -z -c @${IDA}/til-cfg/gcc.cfg -I. -Iempty -b${IDA}/til/gnucmn.til -hopenssl_all.h -t'OpenSSL 1.0.2g (gcc/x86)' openssl_102g_gcc_x86.til
+rm -f openssl_102h_gcc_x86.til openssl_102h_gcc_x64.til
+
+rm -f openssl_all.i
+${IDA}/tilib -Gn -v -z -c @gcc32.cfg -I. -Iempty -b${IDA}/til/gnucmn.til \
+	-hopenssl_all.h -t'OpenSSL 1.0.2h (gcc/x86)' openssl_102h_gcc_x86.til
+rm -f openssl_all.i
+${IDA}/tilib64 -Gn -v -z -c @gcc64.cfg -I. -Iempty -b${IDA}/til/gnucmn.til \
+	-hopenssl_all.h -t'OpenSSL 1.0.2h (gcc/x64)' openssl_102h_gcc_x64.til
+rm -f openssl_all.i
