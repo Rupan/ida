ea7a979c963551d762add6dbf0e996de4516c355
Michael Mohr <akihana@gmail.com>
2017-10-08 21:00:03 -0700

    Update build_til.sh

--- openssl/build_til.sh
+++ openssl/build_til.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-IDA="/opt/ida-6.95"
+IDA="/opt/ida-7.0"
 if [ ! -d "${IDA}" ]
 then
   echo "Edit the IDA path in this script."
   exit 1
 fi
 
-rm -f openssl_102h_gcc_x86.til openssl_102h_gcc_x64.til
+rm -f openssl_101e_gcc_x86.til openssl_101e_gcc_x64.til
 
 rm -f openssl_all.i
 ${IDA}/tilib -Gn -v -z -c @gcc32.cfg -I. -Iempty -b${IDA}/til/gnucmn.til \
-	-hopenssl_all.h -t'OpenSSL 1.0.2h (gcc/x86)' openssl_102h_gcc_x86.til
+	-Iopenssl-1.0.1e/include -hopenssl_all.h -t'OpenSSL 1.0.1e (gcc/x86)' \
+	openssl_101e_gcc_x86.til
 rm -f openssl_all.i
 ${IDA}/tilib64 -Gn -v -z -c @gcc64.cfg -I. -Iempty -b${IDA}/til/gnucmn.til \
-	-hopenssl_all.h -t'OpenSSL 1.0.2h (gcc/x64)' openssl_102h_gcc_x64.til
+	-Iopenssl-1.0.1e/include -hopenssl_all.h -t'OpenSSL 1.0.1e (gcc/x64)' \
+	openssl_101e_gcc_x64.til
 rm -f openssl_all.i
